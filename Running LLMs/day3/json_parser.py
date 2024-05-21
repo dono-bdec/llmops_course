94b8e0825b257cc3e0acf14c212f85f8ca89f267
vishwaskuk <144672885+vishwaskuk@users.noreply.github.com>
2024-05-21 11:46:48 +0530

    Update json_parser.py

--- Running LLMs/day3/json_parser.py
+++ Running LLMs/day3/json_parser.py
@@ -11,7 +11,7 @@ from langchain_core.pydantic_v1 import BaseModel, Field
 from langchain_openai import AzureChatOpenAI
 
 model = AzureChatOpenAI(temperature=0.1,
-                      api_key="39f2089f6a2a4c52967260b0e9ffdc33",
+                      api_key="",
                       api_version="2024-02-01",
                       azure_endpoint="https://dono-rag-demo-resource-instance.openai.azure.com",
                       model="GPT_35_TURBO_DEMO_RAG_DEPLOYMENT_DONO")
